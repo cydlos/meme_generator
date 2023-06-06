d827a370dac47091ace2f44f2c34009ed98ce925
cyd losekann <cydnel@gmail.com>
2023-06-06 05:41:38 -0300

    adds random meme

---
+++ meme_generator/src/Components/Meme.js
@@ -0,0 +1,33 @@
+import memesData from "../memesData";
+
+function Meme() {
+    const handleClick = () => {
+        const randomMeme = memesData[Math.floor(Math.random() * memesData.length)];
+        console.log(randomMeme);
+    };
+
+  return (
+    <main>
+    <div className="form">
+        <input
+            type="text"
+            placeholder="Top text"
+            className="form--input"
+        />
+        <input
+            type="text"
+            placeholder="Bottom text"
+            className="form--input"
+        />
+        <button
+            className="form--button"
+            onClick={handleClick}
+        >
+            Get a new meme image 🖼
+        </button>
+    </div>
+    </main>
+  );
+}
+
+export default Meme;
