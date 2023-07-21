a88d7c16bcfa471609e2546dfca1d6b043a26ebc
cyd losekann <cydnel@gmail.com>
2023-07-21 06:53:48 -0300

    falta o css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,11 +3,11 @@ import React from "react"
 export default function Header() {
     return (
         <header className="header">
-            {/* <img
+            <img
                 alt=""
                 src="./images/troll-face.png"
                 className="header--image"
-            /> */}
+            />
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course - Project 3</h4>
         </header>
